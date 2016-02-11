53b3f5869fdc76b282d92132c268734e00af3ab4
mindymallory <mallorym@illinois.edu>
2016-02-11 17:02:04 -0600

    fdf

--- summary.R
+++ summary.R
@@ -1,23 +1,23 @@
 # Summary tables 
 #This script creates the summary tables for the BBOBAS paper. I'm doing it in a separate script because I recently learned about data.table
 #which will make thie process a lot less painful. I think it will be easier than slogging along with the Analysis.R code. 
-# library(timeDate)
-# library(data.table)
-# library(devtools)
-# library(readr)
-# install_github("<ProfMalloryResearch>/<BBOToolkit>", auth_token = Sys.getenv("GITHUB_PAT"))   # My repository under development
-# library(BBOTools)
-#library(magrittr)
+library(timeDate)
+library(data.table)
+library(devtools)
+library(readr)
+#install_github("<ProfMalloryResearch>/<BBOToolkit>", auth_token = Sys.getenv("GITHUB_PAT"))   # My repository under development
+library(BBOTools)
+library(magrittr)
 
 # 'C:/Users/mallorym/BBOCORNDATA/' # Path to data files on my local computer.
 
 # Defines the dates of this paper's sample, and removes the required dates.
 yearstart <- 2008
-yearend <- 2008
+yearend <- 2011
 #yearend <-  2008
 dates <- timeSequence(from = paste(yearstart, "-01-14", sep = ""), 
-                      #to = paste(yearend, "-11-04", sep = ""))
-                      to = paste(yearend, "-01-16", sep = ""))
+                      to = paste(yearend, "-12-30", sep = ""))
+                      #to = paste(yearend, "-01-20", sep = ""))
 
 
 
@@ -39,87 +39,32 @@ dates <- subset(dates, dates != c('110630')) # Planted Acres report
 dates <- subset(dates, dates != c('110705')) # Light trade after the 4th holiday. No trades or quotes for '3 deferred' which
 # would have been the March 2012 contract.
 dates <- subset(dates, dates != c('80707')) # After 4th holiday, Informa came out with larger than WASDE forecast yeild. \
-dates <- subset(dates, dates != c('90102')) 
+#dates <- subset(dates, dates != c('90102')) 
 dates <- subset(dates, dates != c('90103'))
 dates <- subset(dates, dates != c('90104'))
-dates <- subset(dates, dates != c('90105'))
-dates <- subset(dates, dates != c('90106'))
-dates <- subset(dates, dates != c('90107'))
-dates <- subset(dates, dates != c('90108'))
-dates <- subset(dates, dates != c('90109'))
+#dates <- subset(dates, dates != c('90105'))
+#dates <- subset(dates, dates != c('90106'))
+#dates <- subset(dates, dates != c('90107'))
+#dates <- subset(dates, dates != c('90108'))
+#dates <- subset(dates, dates != c('90109'))
 dates <- subset(dates, dates != c('90110'))
 dates <- subset(dates, dates != c('90111'))
-dates <- subset(dates, dates != c('90112'))
-dates <- subset(dates, dates != c('90113')) # No clue why but these dates are missing from the dataset. 
+#dates <- subset(dates, dates != c('90112'))
+#dates <- subset(dates, dates != c('90113')) # No clue why but these dates are missing from the dataset. 
 
-DATASET <- data.table(t(c(1:8)))
-colnames(DATASET) <- c("TradeDate" ,"TradeTime", "TradeSeqNum", "DeliveryDate", "TrQuantity", "TrPrice", "ASKBID", "EntryDate")
+ptm <- proc.time()
+accum <- as.list(NULL)
 
-start <- c(1, 9, 15, 28, 32, 45, 53, 65)
-end   <- c(8, 14, 22, 31, 36, 51, 53, 70)
 for(i in 1:length(dates)) {
-#   temp <- as.data.table(bboread(paste0('C:/Users/mallorym/BBOCORNDATA/', '2008Jan-2010Jan_txt',
-#                                        "/","XCBT_C_FUT_","0", dates[i], ".txt")))
-#   DATASET <- rbind(DATASET,temp)
-  
-  temp <- as.data.table(bboread(paste0('C:/Users/mallorym/BBOCORNDATA/', '2008Jan-2010Jan_txt',
+
+ DATASET <- as.data.table(bboread(paste0('C:/Users/mallorym/BBOCORNDATA/', '2008Jan-2011Dec_txt',
                                        "/","XCBT_C_FUT_","0", dates[i], ".txt")))
-  
-  DATASET <- rbind(DATASET,temp)
+ 
+ #DATASET[, c("TradeDate", "TradeTime") := .(datemanip(DATASET[, 1, with=FALSE]), timemanip(DATASET[, 2, with=FALSE]))]
+ accum[[i]] <- DATASET[, .N , by = .(TradeDate, ASKBID)]
+ #rm(DATASET)
 }
+proc.time() - ptm
 
-
-# Notes to try
-
-DATASET[, .N, by=.(TradeDate, ASKBID)]  # Close, could write one day at a time. 
-
-
-DATASET[.(TradeTime > 92959 && TradeTime < 131500, TradeTime] 
-
-# Trying to work with time as a facator 
-Times <- as.ITime(timeSequence(from = "2010-01-04 00:00:00", to = "2010-01-04 23:59:59", by = '1 sec'))
-hour(Times)
-
-
-# This works!!!!  # Might be faster to manipulate the string to 2001-02-03 format and convert directly to 
-# posixlt format so that we only do one conversion and not two
-datatimes <- timeDate(paste0(as.character(DATASET$TradeDate), as.character(DATASET$TradeTime), format = "%Y%m%d%H%M%S"))
-datetimes <- 
-  paste0(as.character(DATASET$TradeDate), as.character(DATASET$TradeTime)) %>%
-  timeDate(format = "%Y%m%d%H%M%S") %>%
-  as.data.table()
-
-tttttt<- as.POSIXlt(datetimes)
-tttttt$min    # Can use this to build ten minute bin factors
-bins <- round(tttttt$min/10)*10
-# Now subset for 9:13 hour and paste tttttt$hour and bins together.
-
-makebins <- function(x) {    # contribute to the IDateTime package rather than make this wrapper. 
-          datetimes <- paste0(as.character(x$TradeDate), as.character(x$TradeTime)) %>%
-          timeDate(format = "%Y%m%d%H%M%S") %>%
-          as.POSIXlt()
-  
-  bins <- round(datetimes$min/10)*10 
-  
-  bins <- paste0(datetimes$hour, ":", bins)    # Use data.table features to subset hour[ i = 9 through 13 ] quickly. 
-  
-    return(bins)
-  
-}
-
-# Write function to manipulate character string 'TradeDate' to an unambiguous time format. Need a function so 
-# that operation can be vectorized on the large dataset.
-
-
-dates <- 
-  paste0(as.character(DATASET$TradeDate), as.character(DATASET$TradeTime)) %>%
-  timeDate(format = "%Y%m%d%H%M%S") %>%    
-  as.IDate()
-
-
-# Manipulate Date string into Unambiguous format. 
-
-
-DATA[, TrDate := datemanip(DATA[, 1:2, with=FALSE])]  # Just edit to do assignment
-
+data.table::rbindlist(accum)
 
